16c82c204693c0f4a89d7dd38648b77fb295d7f0
J K <kj688895@gmail.com>
2020-09-26 01:47:00 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,8 +1,9 @@
 require 'pry'
 class Transfer
-  
+  attr_accessor :status
+  attr_reader :transfer, :sender, :receiver
 
-  def initialize
+  def initialize(transfer, sender, receiver)
   end
 
 end
