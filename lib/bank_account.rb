278d30b38ccd876daaf96b56b66439789aab815e
J K <kj688895@gmail.com>
2020-09-26 01:15:57 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -3,10 +3,14 @@ class BankAccount
   attr_accessor :status
   attr_reader :balance, :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
     @balance = 1000
     @status = "open"
   end
+
+  def deposit
 
 end
