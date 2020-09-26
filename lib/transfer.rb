31c987d9ebf6d3c8d36bf664709d47efbc8bc951
J K <kj688895@gmail.com>
2020-09-26 02:01:03 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -3,7 +3,7 @@ class Transfer
   attr_accessor :status
   attr_reader :amount, :sender, :receiver
 
-  def initialize(amount, sender, receiver)
+  def initialize(amount, sender,receiver)
     @amount = amount
     @sender = sender
   end
