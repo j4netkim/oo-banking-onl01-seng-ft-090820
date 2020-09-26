f6aa72aa9c1065ed6d2e6834521b52a8b1c24999
J K <kj688895@gmail.com>
2020-09-26 01:07:45 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,9 +1,12 @@
 require 'pry'
 class BankAccount
   attr_accessor :name
+  attr_reader :balance
 
   def initialize(name)
     @name = name
-  end 
+    @balance = balance
+    balance = 1000
+  end
 
 end
